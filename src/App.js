993b73be55e3d786108b97f2705c5b8986fcbe69
Kobus <kobus@infinityarc.net>
2021-11-25 15:44:23 +0200

    second

--- src/App.js
+++ src/App.js
@@ -27,6 +27,14 @@ function App() {
     );
 }
 
+let api = `https://rickandmortyapi.com/api/character/?page=1`
+
+useEffect(() => {
+    (async function () {
+        let data = await fetch(api).then((res) => res.json());
+        console.log(data);
+    })();
+}, [api]);
 
 
 export default App;
